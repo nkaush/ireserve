9101433bf7bf761f9829df198a206470dafbbf5e
Mehta, Shalin <snmehta2@illinois.edu>
2022-03-21 19:33:11 -0500

    changed buildings for data 1000 rows

--- data_generation/rooms.py
+++ data_generation/rooms.py
@@ -5,7 +5,15 @@ data = [
   ["Nugent Hall","207 E Gregory Dr, Champaign, IL 61820","Ikenberry Commons"],
   ["Hopkins Hall","1208 S 1st St, Champaign, IL 61820","Ikenberry Commons"],
   ["Wardell Hall","1012 W Illinois St, Urbana, IL 61801","Illinois Street Residence"],
-  ["Townsend Hall","918 W Illinois St, Urbana, IL 61801","Illinois Street Residence"]
+  ["Townsend Hall","918 W Illinois St, Urbana, IL 61801","Illinois Street Residence"],
+  ["Weston Hall","204 E Peabody Dr, Champaign, IL 61820","Ikenberry Commons"],
+  ["Scott Hall","202 E Peabody Dr, Champaign, IL 61820","Ikenberry Commons"],
+  ["Bousfield Hall","1214 S 1st St, Champaign, IL 61820","Ikenberry Commons"],
+  ["Carr Hall","1001 W Pennsylvania Ave, Urbana, IL 61801","Pennsylvania Avenue Residence"],
+  ["Saunders Hall","902 W College Ct, Urbana, IL 61801","Pennsylvania Avenue Residence"],
+  ["Babcock Hall","1002 W College Ct, Urbana, IL 61801","Pennsylvania Avenue Residence"],
+  ["Oglesby Hall","1005 W College Ct, Urbana, IL 61801","Florida Avenue Residence"],
+  ["Trelease Hall","901 W College Ct, Urbana, IL 61801","Florida Avenue Residence"]
 ]
 
 for i, d in enumerate(data):
@@ -16,10 +24,10 @@ import random
 
 print("__________________________")
 
-floors = [4, 4, 4, 12, 5]
+floors = [4, 4, 4, 12, 5, 4, 4, 6, 4, 4, 4, 12, 12]
 rid = 0
-for building_id in range(5):
-  num_rooms_per_floor = random.randint(4, 9)
+for building_id in range(len(data)):
+  num_rooms_per_floor = random.randint(4, 13)
   for floor in range(floors[building_id]):
     rooms = {}
     for _ in range(num_rooms_per_floor):
