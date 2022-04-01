27844ac619c05fb1dd02b162ebbf5b3b306ca2ad
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-04-01 16:50:40 -0500

    Pushing gcgcp test files

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,14 +1,13 @@
-#main.py
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from db import *
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return render_template(
-        "main_page.html"
+def home(): 
+    return render_template(
+        "main_page.html", name="neil"
     )
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+source flask-app/.env
