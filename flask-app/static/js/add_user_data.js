427511ebd0bc9390a54ee4108d85ab25bab60b5c
shalinmehta22 <snmehta2@illinois.edu>
2022-04-03 19:14:22 -0500

    added reservations

--- flask-app/static/js/add_user_data.js
+++ flask-app/static/js/add_user_data.js
@@ -1,33 +1,31 @@
 function add_user_data(event) {
-    const url = "/register"
+    const url = "/register";
     const data = {
-        'FirstName' : document.getElementById('first_name').value,
-        'LastName': document.getElementById('last_name').value,
-        'Email': document.getElementById('email').value,
-        'HashedPassword': document.getElementById('password').value,
+      'FirstName' : document.getElementById('first_name').value,
+      'LastName' : document.getElementById('last_name').value,
+      'Email' : document.getElementById('email').value,
+      'HashedPassword' : document.getElementById('password').value,
     };
-
+  
     console.log(data);
-    console.log(JSON.stringify(data))
-
-    // fetch(url, {
-    //     "method": "POST",
-    //     "mode" : "cors",
-    //     "redirect": "manual",
-    //     "headers": {
-    //         "Content-Type": "application/json"
-    //     },
-    //     "body": JSON.stringify(data)
-    // })
-    // .then(function(response) {
-    //     response.json().then((data) => {
-    //         console.log(data);
-    //         document.getElementById('message').innerHTML = data.message;
-    //     });
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    //     document.getElementById('message').innerHTML = err.message;
-    // });
+  
+    fetch(url, {
+      "method": "POST",
+      "mode" : "cors",
+      "headers": {
+          "Content-Type": "application/json; charset=UTF-8"
+      },
+      "body": JSON.stringify(data)
+    })
+    .then(function(response) {
+      console.log(response.url);
+      if (response.redirected) {
+        window.location.href = response.url;
+      }
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+  
     return false;
-}
+  }
